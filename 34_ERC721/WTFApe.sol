b39c7ce6517c72ff573530cdb91f7b3962649940
xjw <xujianwei.xjw@fliggy.com>
2024-12-29 14:37:53 +0800

    wtf NFT

--- 34_ERC721/WTFApe.sol
+++ 34_ERC721/WTFApe.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// by 0xAA
+pragma solidity ^0.8.21;
 
-interface IERC721Metadata {
-    function name() external view returns (string memory);
+import "./ERC721.sol";
 
-    function symbol() external view returns (string memory);
+contract WTFApe is ERC721{
+    uint public MAX_APES = 10000; // 总量
 
-    function tokenURI(uint256 tokenId) external view returns (string memory);
+    // 构造函数
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_){
+    }
+
+    //BAYC的baseURI为ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/ 
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    }
+    
+    // 铸造函数
+    function mint(address to, uint tokenId) external {
+        require(tokenId >= 0 && tokenId < MAX_APES, "tokenId out of range");
+        _mint(to, tokenId);
+    }
 }
