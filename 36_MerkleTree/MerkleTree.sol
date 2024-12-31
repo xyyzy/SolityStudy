374612053f739aaa894abb4bd545dc4ffde20e5a
xjw <xujianwei.xjw@fliggy.com>
2024-12-31 17:46:40 +0800

    feat:merkleTree

--- 36_MerkleTree/MerkleTree.sol
+++ 36_MerkleTree/MerkleTree.sol
@@ -65,15 +65,46 @@ library MerkleProof {
     }
 }
 
-contract MerlkeTree is ERC721{
-    bytes32 immutable public root;
+contract MerlkeTree is ERC721 {
+    bytes32 public immutable root;
     mapping(address => bool) public mintedAddress;
-    constructor(string memory name, string memory symbol, bytes32 merkleroot)
-    ERC721(name, symbol) {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        bytes32 merkleroot
+    ) ERC721(name, symbol) {
         root = merkleroot;
     }
 
-    function mint(address account, uint256)
+    function mint(
+        address account,
+        uint256 tokenId,
+        bytes32[] calldata proof
+    ) public {
+        require(!mintedAddress[account], "Address already minted"); // Merkle检验通过
+        require(
+            MerkleProof.verify(
+                proof,
+                root,
+                keccak256(abi.encodePacked(account))
+            ),
+            "Invalid proof"
+        ); // 地址没有mint过
+        _mint(account, tokenId);
+        mintedAddress[account] = true;
+        
+    }
 
+    function _leaf(address account) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(account));
+    }
 
+    // Merkle树验证，调用MerkleProof库的verify()函数
+    function _verify(
+        bytes32 leaf,
+        bytes32[] memory proof
+    ) internal view returns (bool) {
+        return MerkleProof.verify(proof, root, leaf);
+    }
 }
